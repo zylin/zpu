10995e1545e11556e84665ff013313f5160f6161
oharboe <oharboe>
2008-08-07 13:23:43 +0000

    add missing defs.

--- zpu/hdl/zpu4/core/zpu_config.vhd
+++ zpu/hdl/zpu4/core/zpu_config.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
 
 package zpu_config is
 	-- generate trace output or not.
@@ -12,5 +13,10 @@ package zpu_config is
 	constant	ZPU_Frequency		: std_logic_vector(7 downto 0) := x"64";
 	-- This is the msb address bit. bytes=2^(maxAddrBitIncIO+1)
 	constant 	maxAddrBitIncIO		: integer := 15;
+	constant 	maxAddrBitBRAM		: integer := 14;
 	
+	-- start byte address of stack. 
+	-- point to top of RAM - 2*words
+	constant 	spStart				: std_logic_vector(maxAddrBitIncIO downto 0) :=
+					conv_std_logic_vector((2**(maxAddrBitBRAM+1))-8, maxAddrBitIncIO+1);
 end zpu_config;
