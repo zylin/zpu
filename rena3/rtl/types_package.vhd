e3956729df58f01ca6e2e3b08e5426abde796eac
Bert Lange <b.lange@fzd.de>
2010-08-03 09:58:01 +0200

    change: to sync reset add: default constants

--- rena3/rtl/types_package.vhd
+++ rena3/rtl/types_package.vhd
@@ -10,6 +10,13 @@ package types_package is
         sout   : std_ulogic;
         tout   : std_ulogic;
     end record rena3_controller_in_t;
+    constant default_rena3_controller_in_c : rena3_controller_in_t := (
+        ts    => '0',
+        tf    => '0',
+        fout  => '0',
+        sout  => '0',
+        tout  => '0'
+    );
 
     type rena3_controller_out_t is record
         cshift  : std_ulogic;
@@ -26,6 +33,21 @@ package types_package is
         clf     : std_ulogic;
         tclk    : std_ulogic;
     end record rena3_controller_out_t;
+    constant default_rena3_controller_out_c: rena3_controller_out_t := (
+        cshift  => '0', 
+        cin     => '0', 
+        cs      => '0',
+        read    => '0',
+        tin     => '0',
+        sin     => '0',
+        fin     => '0',
+        shrclk  => '0',
+        fhrclk  => '0',
+        acquire => '0',
+        cls     => '0',
+        clf     => '0',
+        tclk    => '0'
+    );
 
 end package types_package;
 
