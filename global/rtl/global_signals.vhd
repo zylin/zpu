355b80e89a47e1b90a1269fffd95f45c5284f040
Bert Lange <b.lange@fzd.de>
2010-08-13 14:55:56 +0200

    add global package

---
+++ global/rtl/global_signals.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package global_signals is
+
+    type debug_signals_t is record
+        r_ctrl_txen  : std_ulogic;
+        r_txcnt      : std_ulogic_vector(1 downto 0);
+        tmsti_grant  : std_ulogic;
+        tmsti_ready  : std_ulogic;
+        hgrant_0     : std_ulogic;
+        hgrant_1     : std_ulogic;
+        txdstate     : std_ulogic_vector(3 downto 0);
+        ahbmo0_bureq : std_ulogic;
+        ahbmo1_bureq : std_ulogic;
+    end record;
+
+    signal global_break    : std_ulogic;
+
+end package global_signals;
