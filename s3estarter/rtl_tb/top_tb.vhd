e840879b39532f777e0fd828e13a06aa009743d7
Bert Lange <b.lange@fzd.de>
2010-08-23 17:06:55 +0200

    add: memory model and clk_fb

--- s3estarter/rtl_tb/top_tb.vhd
+++ s3estarter/rtl_tb/top_tb.vhd
@@ -25,37 +25,37 @@ architecture testbench of top_tb is
     signal  tb_RESET           : std_ulogic;
     signal  tb_RESET_n         : std_ulogic;
 
-            -- ==== Analog-to-Digital Converter (ADC) ====
-            -- some connections shared with SPI Flash, DAC, ADC, and AMP
+    -- ==== Analog-to-Digital Converter (ADC) ====
+    -- some connections shared with SPI Flash, DAC, ADC, and AMP
     signal  tb_AD_CONV         : std_logic;
 
-            -- ==== Programmable Gain Amplifier (AMP) ====
-            -- some connections shared with SPI Flash, DAC, ADC, and AMP
+    -- ==== Programmable Gain Amplifier (AMP) ====
+    -- some connections shared with SPI Flash, DAC, ADC, and AMP
     signal  tb_AMP_CS          : std_logic;
     signal  tb_AMP_DOUT        : std_logic;
     signal  tb_AMP_SHDN        : std_logic;
 
-            -- ==== Pushbuttons (BTN) ====
+    -- ==== Pushbuttons (BTN) ====
     signal  tb_BTN_EAST        : std_logic;
     signal  tb_BTN_NORTH       : std_logic;
     signal  tb_BTN_SOUTH       : std_logic;
     signal  tb_BTN_WEST        : std_logic;
 
-            -- ==== Clock inputs (CLK) ====
+    -- ==== Clock inputs (CLK) ====
     signal  tb_CLK_50MHZ       : std_logic := '0';
                           
     signal  tb_CLK_AUX         : std_logic := '0';
     signal  tb_CLK_SMA         : std_logic := '0';
 
-            -- ==== Digital-to-Analog Converter (DAC) ====
-            -- some connections shared with SPI Flash, DAC, ADC, and AMP
+    -- ==== Digital-to-Analog Converter (DAC) ====
+    -- some connections shared with SPI Flash, DAC, ADC, and AMP
     signal  tb_DAC_CLR         : std_logic;
     signal  tb_DAC_CS          : std_logic;
 
-            -- ==== 1-Wire Secure EEPROM (DS)
+    -- ==== 1-Wire Secure EEPROM (DS)
     signal  tb_DS_WIRE         : std_logic;
 
-            -- ==== Ethernet PHY (E) ====
+    -- ==== Ethernet PHY (E) ====
     signal  tb_E_COL           : std_logic;
     signal  tb_E_CRS           : std_logic;
     signal  tb_E_MDC           : std_logic;
@@ -69,7 +69,7 @@ architecture testbench of top_tb is
     signal  tb_E_TXD           : std_logic_vector(7 downto 0) := (others => '0');
     signal  tb_E_TX_ER         : std_logic;
 
-            -- ==== FPGA Configuration Mode, INIT_B Pins (FPGA) ====
+    -- ==== FPGA Configuration Mode, INIT_B Pins (FPGA) ====
     signal  tb_FPGA_M0         : std_logic;
     signal  tb_FPGA_M1         : std_logic;
     signal  tb_FPGA_M2         : std_logic;
@@ -77,65 +77,65 @@ architecture testbench of top_tb is
     signal  tb_FPGA_RDWR_B     : std_logic;
     signal  tb_FPGA_HSWAP      : std_logic;
 
-            -- ==== FX2 Connector (FX2) ====
+    -- ==== FX2 Connector (FX2) ====
     signal  tb_FX2_CLKIN       : std_logic;
     signal  tb_FX2_CLKIO       : std_logic;
     signal  tb_FX2_CLKOUT      : std_logic;
 
-            -- These four connections are shared with the J1 6-pin accessory header
-            --FX2_IO          : inout std_logic_vector(4 downto 1);
+    -- These four connections are shared with the J1 6-pin accessory header
+    --FX2_IO          : inout std_logic_vector(4 downto 1);
 
-            -- These four connections are shared with the J2 6-pin accessory header
-            --FX2_IO          : inout std_logic_vector(8 downto 5);
+    -- These four connections are shared with the J2 6-pin accessory header
+    --FX2_IO          : inout std_logic_vector(8 downto 5);
 
-            -- These four connections are shared with the J4 6-pin accessory header
-            --FX2_IO          : inout std_logic_vector(12 downto 9);
+    -- These four connections are shared with the J4 6-pin accessory header
+    --FX2_IO          : inout std_logic_vector(12 downto 9);
 
-            -- The discrete LEDs are shared with the following 8 FX2 connections
-            --FX2_IO        : inout std_logic_vector(20 downto 13);
-            --FX2_IO          : inout std_logic_vector(40 downto 21);
+    -- The discrete LEDs are shared with the following 8 FX2 connections
+    --FX2_IO        : inout std_logic_vector(20 downto 13);
+    --FX2_IO          : inout std_logic_vector(40 downto 21);
     signal  tb_FX2_IO          : std_logic_vector(40 downto 1);
 
-            -- ==== 6-pin header J1 ====
-            -- These are shared connections with the FX2 connector
-            --J1            : inout std_logic_vector(3 downto 0);
+    -- ==== 6-pin header J1 ====
+    -- These are shared connections with the FX2 connector
+    --J1            : inout std_logic_vector(3 downto 0);
 
-            -- ==== 6-pin header J2 ====
-            -- These are shared connections with the FX2 connector
-            --J2            : inout std_logic_vector(3 downto 0);
+    -- ==== 6-pin header J2 ====
+    -- These are shared connections with the FX2 connector
+    --J2            : inout std_logic_vector(3 downto 0);
 
-            -- ==== 6-pin header J4 ====
-            -- These are shared connections with the FX2 connector
-            --J4            : inout std_logic_vector(3 downto 0);
+    -- ==== 6-pin header J4 ====
+    -- These are shared connections with the FX2 connector
+    --J4            : inout std_logic_vector(3 downto 0);
 
-            -- ==== Character LCD (LCD) ====
+    -- ==== Character LCD (LCD) ====
     signal  tb_LCD_E           : std_logic;
     signal  tb_LCD_RS          : std_logic;
     signal  tb_LCD_RW          : std_logic;
 
-            -- LCD data connections are shared with StrataFlash connections SF_D<11:8>
-            --SF_D          : inout std_logic_vector(11 downto 8);
+    -- LCD data connections are shared with StrataFlash connections SF_D<11:8>
+    --SF_D          : inout std_logic_vector(11 downto 8);
 
-            -- ==== Discrete LEDs (LED) ====
-            -- These are shared connections with the FX2 connector
+    -- ==== Discrete LEDs (LED) ====
+    -- These are shared connections with the FX2 connector
     signal  tb_LED             : std_logic_vector(7 downto 0);
 
-            -- ==== PS/2 Mouse/Keyboard Port (PS2) ====
+    -- ==== PS/2 Mouse/Keyboard Port (PS2) ====
     signal  tb_PS2_CLK         : std_logic;
     signal  tb_PS2_DATA        : std_logic;
 
-            -- ==== Rotary Pushbutton Switch (ROT) ====
+    -- ==== Rotary Pushbutton Switch (ROT) ====
     signal  tb_ROT_A           : std_logic;
     signal  tb_ROT_B           : std_logic;
     signal  tb_ROT_CENTER      : std_logic;
 
-            -- ==== RS-232 Serial Ports (RS232) ====
+    -- ==== RS-232 Serial Ports (RS232) ====
     signal  tb_RS232_DCE_RXD   : std_logic;
     signal  tb_RS232_DCE_TXD   : std_logic;
     signal  tb_RS232_DTE_RXD   : std_logic;
     signal  tb_RS232_DTE_TXD   : std_logic;
 
-            -- ==== DDR SDRAM (SD) ==== (I/O Bank 3, VCCO=2.5V)
+    -- ==== DDR SDRAM (SD) ==== (I/O Bank 3, VCCO=2.5V)
     signal  tb_SD_A            : std_logic_vector(12 downto 0);
     signal  tb_SD_BA           : std_logic_vector(1 downto 0);
     signal  tb_SD_CAS          : std_logic;
@@ -150,11 +150,14 @@ architecture testbench of top_tb is
     signal  tb_SD_UDM          : std_logic;
     signal  tb_SD_UDQS         : std_logic;
     signal  tb_SD_WE           : std_logic;
+    -- additional form combining signals
+    signal tb_SD_DM            : std_logic_vector(1 downto 0);
+    signal tb_SD_DQS           : std_logic_vector(1 downto 0);
 
-            -- Path to allow connection to top DCM connection
+    -- Path to allow connection to top DCM connection
     signal  tb_SD_CK_FB        : std_logic;
 
-            -- ==== Intel StrataFlash Parallel NOR Flash (SF) ====
+    -- ==== Intel StrataFlash Parallel NOR Flash (SF) ====
     signal  tb_SF_A            : std_logic_vector(24 downto 0);
     signal  tb_SF_BYTE         : std_logic;
     signal  tb_SF_CE0          : std_logic;
@@ -163,25 +166,25 @@ architecture testbench of top_tb is
     signal  tb_SF_STS          : std_logic;
     signal  tb_SF_WE           : std_logic;
 
-            -- ==== STMicro SPI serial Flash (SPI) ====
-            -- some connections shared with SPI Flash, DAC, ADC, and AMP
+    -- ==== STMicro SPI serial Flash (SPI) ====
+    -- some connections shared with SPI Flash, DAC, ADC, and AMP
     signal  tb_SPI_MISO        : std_logic;
     signal  tb_SPI_MOSI        : std_logic;
     signal  tb_SPI_SCK         : std_logic;
     signal  tb_SPI_SS_B        : std_logic;
     signal  tb_SPI_ALT_CS_JP11 : std_logic;
 
-            -- ==== Slide Switches (SW) ====
+    -- ==== Slide Switches (SW) ====
     signal  tb_SW              : std_logic_vector(3 downto 0);
 
-            -- ==== VGA Port (VGA) ====
+    -- ==== VGA Port (VGA) ====
     signal  tb_VGA_BLUE        : std_logic;
     signal  tb_VGA_GREEN       : std_logic;
     signal  tb_VGA_HSYNC       : std_logic;
     signal  tb_VGA_RED         : std_logic;
     signal  tb_VGA_VSYNC       : std_logic;
 
-            -- ==== Xilinx CPLD (XC) ====
+    -- ==== Xilinx CPLD (XC) ====
     signal  tb_XC_CMD          : std_logic_vector(1 downto 0);
     signal  tb_XC_CPLD_EN      : std_logic;
     signal  tb_XC_D            : std_logic_vector(2 downto 0);
@@ -206,7 +209,9 @@ begin
     tb_BTN_SOUTH  <= '0';
     tb_BTN_WEST   <= '0';
 
-    
+
+    tb_SD_CK_FB   <= transport tb_SD_CK_P after 3 ns; -- time is wild guess
+        
     top_i0: top
         port map (
             AD_CONV         => tb_AD_CONV         , -- : inout std_ulogic;
@@ -289,7 +294,7 @@ begin
             SD_UDQS         => tb_SD_UDQS         , -- : inout std_ulogic;
             SD_WE           => tb_SD_WE           , -- : inout std_ulogic;
 
-            SD_CK_FB        => tb_SD_CK_FB        , -- : inout std_ulogic;
+            SD_CK_FB        => tb_SD_CK_FB        , -- : in    std_ulogic;
 
             SF_A            => tb_SF_A(23 downto 0), -- : inout std_ulogic_vector(24 downto 0);
             SF_BYTE         => tb_SF_BYTE         , -- : inout std_ulogic;
@@ -321,17 +326,9 @@ begin
             GCLK10          => tb_GCLK10            -- : inout std_ulogic
         );
 
-    -- phy emulator
+    -- phy emulator model
     tb_E_MDIO     <= 'H'; -- pullup
---  tb_E_RXD      <= (others => '0');
---  
---  tb_E_COL      <= '0';
---  tb_E_CRS      <= '0';
---  tb_E_RX_CLK   <= '0';
---  tb_E_RX_DV    <= '0';
---  tb_E_RX_ER    <= '0';
---  tb_E_TX_CLK   <= '0';
-
+    
     phy_i0: entity work.phy
         generic map (
             address        => 31, -- : integer range 0 to 31 := 0;
@@ -355,6 +352,50 @@ begin
             mdc            => tb_E_MDC,          -- : in std_logic;
             gtx_clk        => tb_GTX_CLK         -- : in std_logic  
         );
+
+    -- ddr ram model
+    mt46v16m16_i0: entity work.mt46v16m16
+        generic map (                                  -- Timing for -75Z CL2
+--          tCK                 : time    :=  7.500 ns;
+--          tCH                 : time    :=  3.375 ns;       -- 0.45*tCK
+--          tCL                 : time    :=  3.375 ns;       -- 0.45*tCK
+--          tDH                 : time    :=  0.500 ns;
+--          tDS                 : time    :=  0.500 ns;
+--          tIH                 : time    :=  0.900 ns;
+--          tIS                 : time    :=  0.900 ns;
+--          tMRD                : time    := 15.000 ns;
+--          tRAS                : time    := 40.000 ns;
+--          tRAP                : time    := 20.000 ns;
+--          tRC                 : time    := 65.000 ns;
+--          tRFC                : time    := 75.000 ns;
+--          tRCD                : time    := 20.000 ns;
+--          tRP                 : time    := 20.000 ns;
+--          tRRD                : time    := 15.000 ns;
+--          tWR                 : time    := 15.000 ns;
+--          addr_bits           : integer := 13;
+--          data_bits           : integer := 16;
+            cols_bits => 10  -- : integer :=  9;
+--          index               : integer :=  0;
+--          fname               : string  := "sdram.srec";	-- File to read from
+--          bbits               : integer :=  16
+        )
+        port map (
+            Dq    => tb_SD_DQ,        -- : INOUT STD_LOGIC_VECTOR (data_bits - 1 DOWNTO 0) := (OTHERS => 'Z');
+            Dqs   => tb_SD_DQS,       -- : INOUT STD_LOGIC_VECTOR (1 DOWNTO 0) := "ZZ";
+            Addr  => tb_SD_A,         -- : IN    STD_LOGIC_VECTOR (addr_bits - 1 DOWNTO 0);
+            Ba    => tb_SD_BA,        -- : IN    STD_LOGIC_VECTOR (1 DOWNTO 0);
+            Clk   => tb_SD_CK_P,      -- : IN    STD_LOGIC;
+            Clk_n => tb_SD_CK_N,      -- : IN    STD_LOGIC;
+            Cke   => tb_SD_CKE,       -- : IN    STD_LOGIC;
+            Cs_n  => tb_SD_CS,        -- : IN    STD_LOGIC;
+            Ras_n => tb_SD_RAS,       -- : IN    STD_LOGIC;
+            Cas_n => tb_SD_CAS,       -- : IN    STD_LOGIC;
+            We_n  => tb_SD_WE,        -- : IN    STD_LOGIC;
+            Dm    => tb_SD_DM         -- : IN    STD_LOGIC_VECTOR (1 DOWNTO 0)
+        );
+
+    tb_SD_DM  <= tb_SD_UDM  & tb_SD_LDM;
+    tb_SD_DQS <= tb_SD_UDQS & tb_SD_LDQS;
 
 
     -------------------- 
