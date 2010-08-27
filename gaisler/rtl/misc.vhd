5ff3f5ec805157f2f8c636b3719eb33655830c23
Bert Lange <b.lange@fzd.de>
2010-08-27 14:38:14 +0200

    change: reenable components

--- gaisler/rtl/misc.vhd
+++ gaisler/rtl/misc.vhd
@@ -519,57 +519,57 @@ end record;
     );
   end component;
 
---component svgactrl
---generic(
---  length      : integer := 384;        -- Fifo-length
---  part        : integer := 128;        -- Fifo-part lenght
---  memtech     : integer := DEFMEMTECH;
---  pindex      : integer := 0;
---  paddr       : integer := 0;
---  pmask       : integer := 16#fff#;
---  hindex      : integer := 0;
---  hirq        : integer := 0;
---  clk0        : integer := 40000;
---  clk1        : integer := 20000;
---  clk2        : integer := 15385;
---  clk3        : integer := 0;
---  burstlen    : integer range 2 to 8 := 8;
---  asyncrst    : integer range 0 to 1 := 0
---  );
---port (
---  rst       : in std_logic;
---  clk       : in std_logic;
---  vgaclk    : in std_logic;
---  apbi      : in apb_slv_in_type;
---  apbo      : out apb_slv_out_type;
---  vgao      : out apbvga_out_type;
---  ahbi      : in  ahb_mst_in_type;
---  ahbo      : out ahb_mst_out_type;
---  clk_sel   : out std_logic_vector(1 downto 0);
---  arst      : in  std_ulogic := '1'
---  );
+  component svgactrl
+  generic(
+    length      : integer := 384;        -- Fifo-length
+    part        : integer := 128;        -- Fifo-part lenght
+    memtech     : integer := DEFMEMTECH;
+    pindex      : integer := 0;
+    paddr       : integer := 0;
+    pmask       : integer := 16#fff#;
+    hindex      : integer := 0;
+    hirq        : integer := 0;
+    clk0        : integer := 40000;
+    clk1        : integer := 20000;
+    clk2        : integer := 15385;
+    clk3        : integer := 0;
+    burstlen    : integer range 2 to 8 := 8;
+    asyncrst    : integer range 0 to 1 := 0
+    );
+  port (
+    rst       : in std_logic;
+    clk       : in std_logic;
+    vgaclk    : in std_logic;
+    apbi      : in apb_slv_in_type;
+    apbo      : out apb_slv_out_type;
+    vgao      : out apbvga_out_type;
+    ahbi      : in  ahb_mst_in_type;
+    ahbo      : out ahb_mst_out_type;
+    clk_sel   : out std_logic_vector(1 downto 0);
+    arst      : in  std_ulogic := '1'
+    );
 
---end component;
+  end component;
 
   constant vgao_none : apbvga_out_type :=
 	('0', '0', '0', '0', "00000000", "00000000", "00000000", "00");
   constant ps2o_none : ps2_out_type := ('1', '1', '1', '1');
 
---  component ahbrom
---  generic (
---    hindex  : integer := 0;
---    haddr   : integer := 0;
---    hmask   : integer := 16#fff#;
---    pipe    : integer := 0;
---    tech    : integer := 0;
---    kbytes  : integer := 1);
---  port (
---    rst     : in  std_ulogic;
---    clk     : in  std_ulogic;
---    ahbsi   : in  ahb_slv_in_type;
---    ahbso   : out ahb_slv_out_type
---  );
---  end component;
+    component ahbrom
+    generic (
+      hindex  : integer := 0;
+      haddr   : integer := 0;
+      hmask   : integer := 16#fff#;
+      pipe    : integer := 0;
+      tech    : integer := 0;
+      kbytes  : integer := 1);
+    port (
+      rst     : in  std_ulogic;
+      clk     : in  std_ulogic;
+      ahbsi   : in  ahb_slv_in_type;
+      ahbso   : out ahb_slv_out_type
+    );
+    end component;
 
   component ahbdma
    generic (
