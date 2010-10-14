147eff342fa01c2c04864d75eb85400bc1642764
Bert Lange <b.lange@fzd.de>
2010-10-14 11:02:43 +0200

    add: enhance helper script

--- set_mod_bits.sh
+++ set_mod_bits.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 find -iname *.vhd -type f -executable -print0 | xargs -0 chmod -x
-find -iname Makefile -type f -executable -print0 | xargs -0 chmod -x
+
+find -iname *.txt -type f -executable -print0 | xargs -0 chmod -x
+find -iname *.pdf -type f -executable -print0 | xargs -0 chmod -x
+
+find -iname *.ucf -type f -executable -print0 | xargs -0 chmod -x
+find -iname *.bmm -type f -executable -print0 | xargs -0 chmod -x
+find -iname *.cmd -type f -executable -print0 | xargs -0 chmod -x
+find -iname *.xise -type f -executable -print0 | xargs -0 chmod -x
+
+find -iname *.log -type f -executable -print0 | xargs -0 chmod -x
 find -iname modelsim.ini -type f -executable -print0 | xargs -0 chmod -x
+
+find -iname Makefile -type f -executable -print0 | xargs -0 chmod -x
