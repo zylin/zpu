cf207f7945099a2b3ac533abd921cb3e7fa28c0b
Bert Lange <b.lange@fzd.de>
2010-10-18 12:55:33 +0200

    add: softwae stuff

--- set_mod_bits.sh
+++ set_mod_bits.sh
@@ -13,4 +13,7 @@ find -iname *.xise -type f -executable -print0 | xargs -0 chmod -x
 find -iname *.log -type f -executable -print0 | xargs -0 chmod -x
 find -iname modelsim.ini -type f -executable -print0 | xargs -0 chmod -x
 
+find -iname *.c -type f -executable -print0 | xargs -0 chmod -x
+find -iname *.h -type f -executable -print0 | xargs -0 chmod -x
+
 find -iname Makefile -type f -executable -print0 | xargs -0 chmod -x
