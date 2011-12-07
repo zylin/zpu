bf75b6436083a07f083bcfc3c42a45a62e99cfc0
Bert Lange <b.lange@hzdr.de>
2011-12-07 16:08:24 +0100

    change: switch to variable build directory

--- rena3/synthese/program_fpga.cmd
+++ rena3/synthese/program_fpga.cmd
@@ -2,8 +2,8 @@ setMode -bscan
 #setCable -port auto
 setCable -port usb21
 Identify
-#assignFile -p 2 -file "xst/top.bit"
-assignFile -p 2 -file "xst/top_update.bit"
+#assignFile -p 2 -file "top.bit"
+assignFile -p 2 -file "top_update.bit"
 Program -p 2 
 closeCable
 quit
