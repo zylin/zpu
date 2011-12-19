140e83863ebde156e9cda2da25b7a9525a65734c
Bert Lange <b.lange@hzdr.de>
2011-12-19 13:58:19 +0100

    change: improve model with frequency, amplitude and offset

--- rena3/rtl_tb/dds_model.vhd
+++ rena3/rtl_tb/dds_model.vhd
@@ -9,11 +9,16 @@ end entity dds_model;
 library ieee;
 use ieee.math_real.sin;
 use ieee.math_real.cos;
+use ieee.math_real.math_pi;
 
 ----------------------------------------
 architecture behave of dds_model is
 
     constant time_scale : time := 1 ns;
+    --
+    constant frequency  : real := 2.0 * math_pi * real( 10_000_000) / real( 1 sec / time_scale); -- 10 MHz
+    constant amplitude  : real := 0.5; -- zero to peak
+    constant offset     : real := 2.5;
 
 begin
 
@@ -23,8 +28,8 @@ begin
         variable t : real;
     begin
         t  := real( now / time_scale);
-        vu <= sin(t);
-        vv <= cos(t);
+        vu <= sin( frequency * t) * amplitude + offset;
+        vv <= cos( frequency * t) * amplitude + offset;
         wait for time_scale;
         if not run then
             wait; --forever
