4e470fd8220ace64b3b96a40e468d4478b8b3af9
Bert Lange <b.lange@hzdr.de>
2012-02-21 17:27:33 +0100

    change: sort directory

--- hw_s3estarter/synthesis/impact.cmd
+++
@@ -1,7 +0,0 @@
-setMode -bs
-setCable -port auto
-Identify -inferir 
-identifyMPM 
-assignFile -p 1 -file "top_update.bit"
-Program -p 1 
-quit
--- hw_s3estarter/synthesis/prog_spi.cmd
+++
@@ -1,8 +0,0 @@
-setMode -bs
-setCable -port auto
-Identify -inferir 
-identifyMPM 
-attachflash -position 1 -spi "M25P16"
-assignfiletoattachedflash -position 1 -file "D:/home/bl5599/projects/s3estarter/syn/top_update.mcs"
-Program -p 1 -dataWidth 1 -spionly -e -v -loadfpga 
-quit
