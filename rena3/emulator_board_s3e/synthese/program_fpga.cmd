fcc43b8227e294ce6f40ff04eb6dc856519eca6d
Bert Lange <b.lange@hzdr.de>
2013-07-23 09:56:37 +0200

    moved to SVN

--- rena3/emulator_board_s3e/synthese/program_fpga.cmd
+++
@@ -1,7 +0,0 @@
-setMode -bscan
-setCable -port auto
-Identify
-assignFile -p 1 -file "top_update.bit"
-Program -p 1 
-closeCable
-quit
--- rena3/synthesis/program_fpga.cmd
+++
@@ -1,8 +0,0 @@
-setMode -bscan
-#setCable -port auto
-setCable -port usb21
-Identify
-assignFile -p 2 -file "top_update.bit"
-Program -p 2 
-closeCable
-quit
